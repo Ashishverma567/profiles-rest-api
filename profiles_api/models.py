964028afcddfb24772521a54b8a2ec1b2149dfc3
Ashish <asverma7768@gmail.com>
2021-10-04 08:59:25 +0530

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+#above 2 are used to override/customize the default jango user model
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profiles"""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)  #will create a new model that UserProfileManager is representing by default self.model is set for model that manager is for then it will create a new model
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save new super user with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True #auto. created by PermisionMixin
+        user.is_staff = True #present in Userprofile class
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True) #check user profile is active by default we keep active
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+#for now for both methods above we are returning names
+
+    def __str__(self):
+        """Return string rep. of our user"""
+        return self.email
+#item we want tor return when we convert UserProfile class object into string in python
